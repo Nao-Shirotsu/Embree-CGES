aed5f87ce430d78534b698e3786fdc556eee3592
Shirotsu <albus0sh@gmail.com>
2019-09-21 10:21:46 +0900

    opengl with glfwも動いた

--- CGES/src/main.cpp
+++ CGES/src/main.cpp
@@ -4,12 +4,48 @@
 
 #include <embree3/rtcore.h>
 #include <embree3/rtcore_ray.h>
+#include <GLFW/glfw3.h>
+#include <GL/GL.h>
+
+void ExecEmbreeRayhit();
+void ExecOpenglProcess();
+
+int main() {
+  ExecEmbreeRayhit();
+  ExecOpenglProcess();
+}
 
 constexpr float INF = std::numeric_limits<float>::max();
 
-int main() {
+void ExecOpenglProcess() {
+  if(glfwInit() == GL_FALSE) {
+    std::cout << "GLFW init failed" << std::endl;
+    return;
+  }
+  GLFWwindow* window = glfwCreateWindow(500, 500, "Test Window", nullptr, nullptr);
+  if(!window) {
+    std::cout << "GL window creation failed" << std::endl;
+    glfwTerminate();
+    return;
+  }
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+  glfwMakeContextCurrent(window);
+  glfwSwapInterval(1);
+
+  while(glfwWindowShouldClose(window) == GL_FALSE) {
+    glClearColor(0.25, 0.25, 0.25, 0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+  glfwTerminate();
+}
+
+void ExecEmbreeRayhit() {
   RTCDevice device = rtcNewDevice(nullptr);
-  if(!device) {
+  if (!device) {
     std::cout << "Device Error : " << rtcGetDeviceError(nullptr) << std::endl;
   }
 
@@ -28,7 +64,7 @@ int main() {
                                                                      RTC_FORMAT_UINT3,
                                                                      3 * sizeof(uint32_t),
                                                                      1));
-  
+
   if (vertices && indices) {
     vertices[0] = 0.f;
     vertices[1] = 0.f;
@@ -65,9 +101,9 @@ int main() {
   rayhit.ray.flags = 0;
   rayhit.hit.geomID = RTC_INVALID_GEOMETRY_ID;
   rayhit.hit.instID[0] = RTC_INVALID_GEOMETRY_ID;
-  
+
   rtcIntersect1(scene, &context, &rayhit);
-  if(rayhit.hit.geomID != RTC_INVALID_GEOMETRY_ID) {
+  if (rayhit.hit.geomID != RTC_INVALID_GEOMETRY_ID) {
     std::cout << "Intersected at" << rayhit.ray.tfar << std::endl;
   }
   else {
