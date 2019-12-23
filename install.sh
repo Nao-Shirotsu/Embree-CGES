914d38dc41860a9a517f03a48a3af503f7724f3a
Shirotsu <albus0sh@gmail.com>
2019-12-23 15:02:20 +0900

    install.shをログファイル出力するように変更

--- install.sh
+++ install.sh
@@ -1,28 +1,62 @@
+#!/bin/zsh
+
 cd CGES
-if [ -d ./embree3 ]; then
-    echo "[CGES] Embree dependencies already installed!"
+# ↓diffとって判定した方がよさそう
+echo "[CGES] Log files about installing will be generated in CGES_HOME/install_log"
+if [ -d ../install_log ]; then
 else
-    echo "[CGES] Download embree dependencies..."
-    wget https://github.com/embree/embree/releases/download/v3.6.1/embree-3.6.1.x86_64.linux.tar.gz
-    echo "[CGES] Install embree dependencies..."
-    tar -xvzf embree-3.6.1.x86_64.linux.tar.gz
+    mkdir ../install_log
+fi
+
+if [ -d ./embree3 ]; then
+    echo "[CGES] Embree dependencies already installed."
+else
+    echo "[CGES] Downloading embree dependencies..."
+    LOG_PATH="../install_log/install_embree.log"
+    if [ -f $LOG_PATH ]; then
+        rm $LOG_PATH
+    fi
+    date >> $LOG_PATH
+    echo  >> $LOG_PATH
+    wget https://github.com/embree/embree/releases/download/v3.6.1/embree-3.6.1.x86_64.linux.tar.gz >> $LOG_PATH
+    echo "[CGES] Installing embree dependencies..."
+    tar -xvzf embree-3.6.1.x86_64.linux.tar.gz >> $LOG_PATH
+    rm embree-3.6.1.x86_64.linux.tar.gz
     mv embree-3.6.1.x86_64.linux embree3
-    ./embree3/embree-vars.sh
 fi
 
 if [ -d ./FindGLFW_Test ]; then
-    echo "[CGES] GLFW .cmake files already installed!"
+    echo "[CGES] GLFW .cmake files already installed."
 else
-    echo "[CGES] Download GLFW .cmake files..."
-    git clone git@github.com:benikabocha/FindGLFW_Test.git
+    LOG_PATH=../install_log/install_findglfw.log
+    if [ -f $LOG_PATH ]; then
+        rm $LOG_PATH
+    fi
+    echo "[CGES] Downloading GLFW .cmake files..."
+    date >> $LOG_PATH
+    echo  >> $LOG_PATH
+    git clone git@github.com:benikabocha/FindGLFW_Test.git >> $LOG_PATH
 fi
 
-if [ -d ./bin ]; then
-    echo "[CGES] cges binary already exists!"
+# ↓diffとって判定した方がよさそう
+if [ -f ./bin/cges ]; then
+    echo "[CGES] cges build already completed!"
 else
-    echo "[CGES] Build cges binaries..."
-    mkdir bin
-    cmake -DCMAKE_BUILD_TYPE=Release .
-    make
+    echo "[CGES] Building cges binaries..."
+    LOG_PATH=../install_log/install_cges.log
+    if [ -f $LOG_PATH ]; then
+        rm $LOG_PATH
+    fi
+    date >> $LOG_PATH
+    echo  >> $LOG_PATH
+    # ↓diffとって判定した方がよさそう
+    if [ -d ./build ]; then
+    else
+        mkdir build
+    fi
+    cd build
+    cmake -DCMAKE_BUILD_TYPE=Release .. >> ../$LOG_PATH
+    make >> ../$LOG_PATH
+    cd ..
 fi
 cd ..
