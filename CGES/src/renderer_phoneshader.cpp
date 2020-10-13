1b5bd35f6c517c36ed926673dcc5b61e3a2939ba
Shirotsu <albus0sh@gmail.com>
2020-10-13 19:09:28 +0900

    phongshaderのdirlightをSceneからRenderer側に移植

--- CGES/src/renderer_phoneshader.cpp
+++ CGES/src/renderer_phoneshader.cpp
@@ -1,8 +1,15 @@
 #include "renderer_phoneshader.hpp"
 #include "util_renderer.hpp"
+#include "color.hpp"
 
 #include <glm/glm.hpp>
 #include <algorithm> // for std::clamp
+
+namespace {
+
+constexpr cges::ColorRGBA EMIT_COLOR = { 100, 100, 100, 255 };
+
+}
 
 namespace cges::renderer {
 PhongShader::PhongShader() : Base(Method::PhongShading){
@@ -27,7 +34,7 @@ void PhongShader::ParallelDraw(const Camera& camera,
       rtcInitIntersectContext(&context);
       const float xRate = x / static_cast<float>(renderTarget.GetWidth());
       const glm::vec3 pixelPos = initialPos + (yRate * screenVerticalVec) + (xRate * screenHorizontalVec);
-      const glm::vec3 rayDir = pixelPos - cameraPos;
+      const glm::vec3 rayDir = glm::normalize(pixelPos - cameraPos);
       RTCRayHit rayhit;
       InitRayHit(rayhit, cameraPos, rayDir);
       rtcIntersect1(scene.GetRTCScene(), &context, &rayhit);
@@ -56,9 +63,10 @@ void PhongShader::ParallelDraw(const Camera& camera,
       else {
         faceNormal = glm::normalize(glm::vec3(rayhit.hit.Ng_x, rayhit.hit.Ng_y, rayhit.hit.Ng_z));
       }
-      const glm::vec3 reflectedDir = scene.GetDirLightForward() - 2.0f * glm::dot(scene.GetDirLightForward(), faceNormal) * faceNormal;
-      const float specularFactor = std::clamp(glm::dot(glm::normalize(reflectedDir), glm::normalize(cameraPos)), 0.0f, 1.0f);
-      const float diffuseFactor = std::clamp(glm::dot(-scene.GetDirLightForward(), faceNormal), 0.0f, 1.0f);
+
+      const glm::vec3 reflectedDir = rayDir - 2.0f * glm::dot(rayDir, faceNormal) * faceNormal;
+      const float specularFactor = std::clamp(std::fabsf(glm::dot(glm::normalize(reflectedDir), glm::normalize(cameraPos))), 0.0f, 1.0f);
+      const float diffuseFactor = std::clamp(std::fabsf(glm::dot(-rayDir, faceNormal)), 0.0f, 1.0f);
       const ColorRGBA objColor = gameObjRef.GetColor(rayhit.hit.u, rayhit.hit.v);
       renderTarget[yIdx][x].r = std::clamp(static_cast<int>(objColor.r * diffuseFactor + 96 * specularFactor) + 32, 0, 255);
       renderTarget[yIdx][x].g = std::clamp(static_cast<int>(objColor.g * diffuseFactor + 96 * specularFactor) + 16, 0, 255);
--- CGES/src/scene.cpp
+++ CGES/src/scene.cpp
@@ -7,12 +7,8 @@ namespace cges {
 Scene::Scene(const RTCDevice rtcDevice)
     : m_rtcScene{ rtcNewScene(rtcDevice) }
     , m_objects( 0 )
-    , m_lightSrcs( 0 )
-    , m_sceneChanged{false} {
-  const float divRoot3 = 1.0f / std::sqrtf(3.0f);
-  m_dirLight.dir = { -divRoot3, -divRoot3, -divRoot3 };
-  m_dirLight.emitColor = { 128, 128, 128, 255 };
-}
+    //, m_lightIdx( 0 )
+    , m_sceneChanged{false} {}
 
 Scene::~Scene() noexcept {
   rtcReleaseScene(m_rtcScene);
@@ -34,10 +30,6 @@ void Scene::Add(std::unique_ptr<gameobject::Base> object) {
 
 const RTCScene Scene::GetRTCScene() const {
   return m_rtcScene;
-}
-
-const glm::vec3& Scene::GetDirLightForward() const {
-  return m_dirLight.dir;
 }
 
 const gameobject::Base& Scene::GetGeomRef(const unsigned int objID) const {
